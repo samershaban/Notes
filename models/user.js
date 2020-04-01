60bef3e2254aa36afe08c05cb7a8dec3295aa81a
Samer Shaban <samershaban@Samers-MBP.fios-router.home>
2020-04-01 19:31:55 -0400

    added new users endpoint

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ function validateUser(user) {
     const schema = {
         name: Joi.string().min(3).max(50).required(),
         email: Joi.string().min(5).max(100).email().required(),
-        password: Jois.string().min(8).max(1024).required()
+        password: Joi.string().min(8).max(1024).required()
     };
     return Joi.validate(user, schema);
 }
