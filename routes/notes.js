dfa9df705e7cadbae0bd54cd1fc93aacadc6355b
Samer Shaban <samershaban@Samers-MBP.fios-router.home>
2020-04-01 16:01:43 -0400

    allowed note duplicates

--- routes/notes.js
+++ routes/notes.js
@@ -12,9 +12,6 @@ router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
   
-    let note = await Note.findOne({ name: req.body.name });
-    if (note) return res.status(400).send('note already registered.');
-  
     note = new Note({ 
         title: req.body.title,
         body: req.body.body 
