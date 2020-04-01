390eb2cbd6709dee47b4b5f0214c10144bb435e7
Samer Shaban <samershaban@Samers-MBP.fios-router.home>
2020-04-01 15:11:26 -0400

    added notes model

---
+++ models/note.js
@@ -0,0 +1,29 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const noteScheme = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 20
+    },
+    body: {
+        type: String,
+        required: true,
+        minLength:1,
+        maxLength: 2048
+    }
+})
+
+const Note = mongoose.model('Note', noteSchema);
+
+function validateNote(note) {
+    const schema = {
+        title: Joi.string().min(2).required(),
+        body: Joi.string().min(1).required()
+    }
+}
+exports.noteSchema = noteSchema;
+exports.Note = Note;
+exports.validate = validateNote;
