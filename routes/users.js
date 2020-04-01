ff81d9ea75159b478109f214244c458320e7ef09
Samer Shaban <samershaban@Samers-MBP.fios-router.home>
2020-04-01 19:53:22 -0400

    used bcrypt to hash passwords

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const config = require('config');
 const _ = require('lodash');
 const {User, validate} = require('../models/user');
@@ -14,6 +15,9 @@ router.post('/', async (req, res) =>{
     if (user) return res.status(400).send('User already registered.');
 
     user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt)
+    
     await user.save();
 
     res.send(_.pick(user, ['_id', 'name', 'email']));
