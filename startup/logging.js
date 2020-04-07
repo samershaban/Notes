20e058aa43da860a84be3c6ae6159ba107f0b93a
Samer Shaban <samershaban@Samers-MBP.fios-router.home>
2020-04-07 14:51:10 -0400

    added console transports to winston

--- startup/logging.js
+++ startup/logging.js
@@ -3,8 +3,9 @@ require('winston-mongodb');
 
 module.exports = function() {
     winston.handleExceptions(
+        new winston.transports.Console({colorize: true, prettyPrint: true}),
         new winston.transports.File({filename: 'uncaughtExceptions.log'})
-    )
+    );
     process.on('unhandledRejection', (e) => {
         throw ex;
     });
